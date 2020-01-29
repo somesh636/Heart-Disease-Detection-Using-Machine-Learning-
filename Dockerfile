89716a65e9950c8b76f170dba6d82f6e43ca011b
Michael Lynch <mikrlynch@gmail.com>
2020-01-28 20:17:26 -0500

    Got docker working

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,13 @@ RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev
 
 COPY ./app/ /app/
-gWORKDIR /app/
+WORKDIR /app/
 
 RUN pip install -r flask-serv-req.txt
 
-ENTRYPOINT [ "python" ]
-CMD [ "app.py" ]
+#ENTRYPOINT [ "python" ]
+#CMD [ "app.py" ]
+CMD [ "flask", "run", "--host", "0.0.0.0" ]
+
+
 
--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 from wtforms import Form, TextAreaField, validators 
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path = "",
+            static_folder = "/static" ,
+            template_folder = "/templates")
 
 class HelloForm(Form):
     sayhello = TextAreaField('', [validators.DataRequired()])
@@ -9,7 +12,7 @@ class HelloForm(Form):
 @app.route('/')
 def index():
     form = HelloForm(request.form)
-    return render_template('first_app.html', form = form)
+    return render_template('index.htm', form = form)
 
 @app.route('/hello', methods=['POST'])
 def hello():
@@ -17,7 +20,7 @@ def hello():
     if request.method == 'POST' and form.validate():
         name = request.form['sayhello']
         return render_template('hello.html', name = name)
-    return render_template('first_app.html', form = form)
+    return render_template('index.htm', form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
